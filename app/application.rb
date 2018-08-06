25c4e81187d4a51ff013bb87fc3788e77184ee5d
JohnMark <johnmarkludwick@gmail.com>
2018-08-06 21:47:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
- 
+    time = time.now
+    
     if time.now>= 12 
       resp.write "Good Morning!"
     else time.now> 12 && timedate<= 24
