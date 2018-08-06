43b98c4f5190b818329689782de2fde66b58cc02
JohnMark <johnmarkludwick@gmail.com>
2018-08-06 21:49:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
  
     time = Time.new
     
-    if time>= 12 
+    if time>= 12
       resp.write "Good Morning!"
     else time> 12 && timedate<= 24
       resp.write "Good Afternoon!"
